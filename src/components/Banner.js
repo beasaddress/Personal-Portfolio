6dbd7d2a62e699fca420733bd7d13d10d49b012e
Beatriz <beasaddress@gmail.com>
2023-11-06 18:07:30 -0800

    adding banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,4 +1,6 @@
-import {Container, Row } from "react-bootstrap";
+import {Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img"
 
 export const Banner = () => {
     return (
@@ -9,7 +11,10 @@ export const Banner = () => {
                         <span className="tagline">Welcome to my Portfolio</span>
                         <h1>{`Hi, I'm webdecoded`}<span classname="wrap">web developer</span></h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tincidunt, nunc auctor euismod malesuada, purus augue tincidunt elit. Fusce suscipit, nisl vel fringilla sagittis, quam justo auctor nisi, ac tincidunt ante libero nec orci. Suspendisse in odio augue. Integer vel viverra nibh.</p>
-                        <button onClick={() => console.log('connect') }></button>
+                        <button onClick={() => console.log('connect')}>Connect With Me <ArrowRightCircle size={25} /></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={} alt="header img" />
                     </Col>
                 </Row>
             </Container>
